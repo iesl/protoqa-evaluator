054ba04fa133ef3adcc3a492408df4feaae6c69d
Michael Boratko <boratko@gmail.com>
2019-11-23 02:44:49 -0500

    fix: General clean-up

--- family_feud_evaluator/data_processing.py
+++ family_feud_evaluator/data_processing.py
@@ -1,11 +1,7 @@
 from pathlib import Path
 from typing import *
 import json
-# import nltk
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
 
-# stop_words = set(stopwords.words('english'))
 
 def load_data_from_jsonl(data_path:Union[Path,str]):
     question_data = dict()
@@ -14,9 +10,6 @@ def load_data_from_jsonl(data_path:Union[Path,str]):
             q_json = json.loads(q)
             question_data[q_json['questionid']] = q_json
     return question_data
-
-# def filter_questions:
-#     nltk.download('punkt') # needed for tokenization
 
 
 def default_string_preprocessing(pred_answer: str, length_limit:int = 50) -> str:
