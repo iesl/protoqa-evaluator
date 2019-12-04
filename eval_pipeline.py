21029d2e66e228784372b9acc816e72eab379652
Michael Boratko <boratko@gmail.com>
2019-12-04 15:33:31 -0500

    fix: Change hard/soft switch

--- eval_pipeline.py
+++ eval_pipeline.py
@@ -1,6 +1,6 @@
 from family_feud_evaluator.evaluation import evaluate, family_feud, fast_money, EvalResult, set_intersection, hard_set_intersection, family_feud_5_incorrect
 from family_feud_evaluator.scoring import longest_common_subsequence_score, longest_common_substring_score, \
-    wordnet_score, bleu_score, meteor_score, exact_match
+    wordnet_score, exact_match
 from functools import partial
 from family_feud_evaluator.data_processing import load_data_from_jsonl, load_predictions
 from collections import defaultdict
@@ -9,16 +9,16 @@ import json
 import argparse
 import numpy as np
 from tabulate import tabulate
+import numpy as np
 
 
 family_feud_wn_sim = partial(family_feud, score_func=wordnet_score)
 fast_money_wn_sim = partial(fast_money, score_func=wordnet_score)
 fast_money_substring_sim = partial(fast_money, score_func=longest_common_substring_score)
-fast_money_bleu_sim = partial(fast_money, score_func=bleu_score)
-fast_money_meteor_sim = partial(fast_money, score_func=meteor_score)
 
-EVAL_METHODS = [("Fast Money", fast_money), ("Family Feud", family_feud), ("Set Intersection", set_intersection), ("Hard Set Intersection", hard_set_intersection), ("Family Feud (5 incorrect)", family_feud_5_incorrect)]
-SIM_FUNCS = [("exact match", exact_match), ("Longest Substr", longest_common_substring_score), ("Longest subseq", longest_common_subsequence_score), ("Wordnet", wordnet_score)]
+EVAL_METHODS = [("Fast Money", fast_money), ("Family Feud", family_feud), ("Set Intersection", set_intersection), ("Family Feud (5 incorrect)", family_feud_5_incorrect)]
+SIM_FUNCS = [("Exact Match", exact_match), ("Longest Substr", longest_common_substring_score), ("Longest Subseq", longest_common_subsequence_score), ("WordNet", wordnet_score)]
+HARD_SOFT = [("Hard", np.round), ("Soft", None)]
 
 
 
@@ -28,15 +28,17 @@ def calc_scores(predictions: Dict[str, List[str]], question_data: Dict) -> float
     for eval_method in EVAL_METHODS:
         rows = [eval_method[0]]
         for sim_func in SIM_FUNCS:
-            print("Eval method: {}".format(eval_method[0]))
-            print("Similarity Function: {}".format(sim_func[0]))
-            method = partial(eval_method[1], score_func=sim_func[1])
-            scores = evaluate(method, question_data=question_data, answers_dict=predictions)
-            avg_score = np.mean([s.score for _, s in scores.items()])
-            # for _, s in scores.items():
-            #     print(s.answer_assignment)
-            # print(avg_score)
-            rows.append(avg_score)
+            for hs in HARD_SOFT:
+                print("Eval method: {}".format(eval_method[0]))
+                print("Similarity Function: {}".format(sim_func[0]))
+                print("Hard/Soft: {}".format(hs[0]))
+                method = partial(eval_method[1], score_func=sim_func[1], score_matrix_transformation=hs[1])
+                scores = evaluate(method, question_data=question_data, answers_dict=predictions)
+                avg_score = np.mean([s.score for _, s in scores.items()])
+                # for _, s in scores.items():
+                #     print(s.answer_assignment)
+                # print(avg_score)
+                rows.append(avg_score)
         all_rows.append(rows)
     header = ["Eval method"]
     for s in SIM_FUNCS:
